61a24816aa1f00dea9b99c394e4067ea060c39b8
d0h <dev1lc3st@gmail.com>
2025-03-17 20:53:37 +0300

    нихкя, это был не репоз всё это время

---
+++ LB_2/app/src/main/java/com/example/laba_2_rmp/Ishgl.java
@@ -0,0 +1,60 @@
+package com.example.laba_2_rmp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Ishgl extends AppCompatActivity {
+
+    private GestureDetector gestureDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ishgl);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnOpenTours = findViewById(R.id.button);
+
+        btnOpenTours.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Ishgl.this, scrll_ishgli.class);
+                startActivity(intent);
+            }
+        });
+
+        // Добавляем обработку свайпа
+        gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                if (e1.getX() > e2.getX()) {
+                    // Свайп влево — переход на Cermat
+                    Intent intent = new Intent(Ishgl.this, cermat.class);
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event) || super.onTouchEvent(event);
+    }
+}
---
+++ LB_2/app/src/main/java/com/example/laba_2_rmp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.laba_2_rmp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btnOpenTours = findViewById(R.id.btnOpenTours);
+
+        btnOpenTours.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Ishgl.class);
+                startActivity(intent);
+                }
+            }
+        );
+    }
+}
